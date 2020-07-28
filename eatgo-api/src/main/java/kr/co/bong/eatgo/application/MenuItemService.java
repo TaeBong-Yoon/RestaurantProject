f0caadfefd7ef22b7c651c6d1a99402d31f8c2d5
TaeBong-Yoon <fkdlwldxogus@gmail.com>
2020-07-28 16:00:25 +0900

    add menu system

---
+++ eatgo-api/src/main/java/kr/co/bong/eatgo/application/MenuItemService.java
@@ -0,0 +1,33 @@
+package kr.co.bong.eatgo.application;
+
+import kr.co.bong.eatgo.domain.MenuItem;
+import kr.co.bong.eatgo.domain.MenuItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    private MenuItemRepository menuItemRepository;
+    @Autowired
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public void bulkUpdate(Long restaurantID, List<MenuItem> menuItems) {
+        for(MenuItem menuItem : menuItems){
+            update(restaurantID, menuItem);
+        }
+    }
+
+    private void update(Long restaurantID, MenuItem menuItem) {
+        if(menuItem.isDestroy()) {
+            menuItemRepository.deleteById(menuItem.getId());
+            return;
+        }
+        menuItem.setRestaurantId(restaurantID);
+        menuItemRepository.save(menuItem);
+    }
+}
---
+++ eatgo-api/src/test/java/kr/co/bong/eatgo/application/MenuItemServiceTests.java
@@ -0,0 +1,50 @@
+package kr.co.bong.eatgo.application;
+
+import kr.co.bong.eatgo.domain.MenuItem;
+import kr.co.bong.eatgo.domain.MenuItemRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class MenuItemServiceTests {
+
+    private MenuItemService menuItemService;
+    @Mock
+    private MenuItemRepository menuItemRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+         menuItemService = new MenuItemService(menuItemRepository);
+    }
+
+    @Test
+    public void bulkUpdate() {
+        List<MenuItem> menuItems = new ArrayList<MenuItem>();
+        menuItems.add(MenuItem.builder()
+                .name("Hot Curry")
+                .build());
+        menuItems.add(MenuItem.builder()
+                .id(12L)
+                .name("Chicken Curry")
+                .build());
+        menuItems.add(MenuItem.builder()
+                .id(1004L)
+                .destroy(true)
+                .build());
+        menuItemService.bulkUpdate(1L, menuItems);
+
+        verify(menuItemRepository, times(2)).save(any());
+        verify(menuItemRepository, times(1)).deleteById(eq(1004L));
+    }
+
+}
